c6ebf4125514d12aaa142f4d86c98ed9d13b39cc
wagabooga <mattyhutwo@gmail.com>
2022-01-03 23:24:10 -0800

    added mock page layout to move on to styling

---
+++ components/Center.js
@@ -0,0 +1,12 @@
+function Center() {
+  return (
+    <div className="bg-white flex-grow">
+      <h1> I am middle</h1>
+      <p>The classic latin passage that just never gets old, enjoy as much (or as little) lorem ipsum as you can handle with our easy to use filler text generator.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+      <h3>Lorem Ipsum is the single greatest threat. We are not - we are not keeping up with other websites. Lorem Ipsum best not make any more threats to your website. </h3>
+    </div>
+  )
+}
+
+export default Center
