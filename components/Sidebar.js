78eaf7c06d1f7b1e4fe6e5366ffd8e6636c33686
wagabooga <mattyhutwo@gmail.com>
2022-01-03 23:06:15 -0800

    added sessions via nextAuth

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -6,21 +6,23 @@ import {
   HeartIcon,
   RssIcon
 } from "@heroicons/react/outline"
-import { signOut } from "next-auth/react"
+import { signOut, useSession } from "next-auth/react"
 
 function Sidebar() {
+  const { data: session, status } = useSession();
+
+  console.log(session)
   return (
     <div className="text-gray-500 p5 text-sm border-r border-gray-900">
+      <div className="space-y-4">
+        <button className="flex items-center space-x-2 hover:text-white" onClick={() => signOut()}>
+          <p>Log out</p>
+        </button>
+      </div>
       <div className="space-y-4">
         <button className="flex items-center space-x-2 hover:text-white">
           <HomeIcon  className="h-5 w-5"/>
           <p>Home</p>
-        </button>
-      </div>
-      <div className="space-y-4">
-        <button className="flex items-center space-x-2 hover:text-white" onClick={() => signOut()}>
-          <HomeIcon  className="h-5 w-5"/>
-          <p>Log out</p>
         </button>
       </div>
       <div>
